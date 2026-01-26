e25b8e813a3b534c1a537e1ed4aba1f915aece99
Moises Aguirre <maguirre8459@floridapoly.edu>
2026-01-26 18:49:54 +0000

    Updated class notes

---
+++ 06_reference_vars/reference_vars.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+void add_five(int& num);
+
+int main(void) {
+    int a = 10;
+
+    // Create a reference variable
+    int& a_ref = a;
+
+    a += 10;
+    std::cout << a << " " << a_ref << std::endl;
+
+    a_ref *= 10;
+    std::cout << a << " " << a_ref << std::endl;
+
+    add_five(a);
+    std::cout << a << " " << a_ref << std::endl;
+
+    return 0;
+}
+
+void add_five(int& num) {
+    num += 5;
+}
