75e59cb7440671dfd04baea6ace00d104590812a
Moises Aguirre <maguirre8459@floridapoly.edu>
2026-02-04 17:06:18 +0000

    Updated program

---
+++ 10_print_combinations/print_combinations.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+void print_combinations(int n);
+void combinations_helper(std::string& s, int n, int i);
+
+int main(void) {
+    std::cout << "Enter size ";
+    int n;
+    std::cin >> n;
+    print_combinations(n);
+
+    return 0;
+}
+
+void print_combinations(int n){
+    std::string s(n, 'a');
+    // Call the helper function
+    combinations_helper(s, n, 0);
+}
+
+void combinations_helper(std::string& s, int n, int i){
+    // Base case
+    if (i >= n) { // Index of last character
+    std::cout << s << std::endl;
+    return;
+    }
+    while (s[i] < 'z') {
+        combinations_helper(s, n, i + 1);
+        s[i]++;
+    }
+    s[i] = 'a';
+}
+
+// To send this output to a text file, type:
+// ./a.out > out.txt
